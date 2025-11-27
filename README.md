e863e321ff3a8f0c5c94a795a83361075e8e2086
denofdhamodhar <dhamodhar459@gmail.com>
2025-11-27 19:52:06 +0530

    Starter Template Of The Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Amazon ( React JS )
+This is the main branch it contain starter template of this project 
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css"
+
+function App() {
+  return (
+    <>
+      <div>
+        <h3 className="text-center py-4 underline-effect">Onboard to the amazon project</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
